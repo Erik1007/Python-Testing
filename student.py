9546374ac00406497eb3fb25b80ee09b8e4589ff
Erik1007 <erik.hartman1007@gmail.com>
2022-10-31 08:36:24 +0000

    testing the life cycle of python testing with set up and tear down classes

--- student.py
+++ student.py
@@ -1,17 +1,23 @@
 from datetime import date, timedelta
 
 
-class student:
-    """A student class as a base for method testing"""
+class Student:
+    """ A Student class as a basis for method testing """
 
-
-def __init__(self, first_name, last_name):
-    self._first_name = first_name
-    self._last_name = last_name
-    self._start_date = date.today()
-    self.end_date = end_date() + timedelta(days=365)
-    self.naughty_list = False
+    def __init__(self, first_name, last_name):
+        self._first_name = first_name
+        self._last_name = last_name
+        self._start_date = date.today()
+        self.end_date = date.today() + timedelta(days=365)
+        self.naughty_list = False
 
     @property
     def full_name(self):
-        return f"{self.first_name} {self.last_name}"
+        return f"{self._first_name} {self._last_name}"
+
+    @property
+    def email(self):
+        return f"{self._first_name.lower()}.{self._last_name.lower()}@email.com"
+
+    def alert_santa(self):
+        self.naughty_list = True
